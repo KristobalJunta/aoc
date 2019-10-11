99579e26710b9627c7290a59805339b0af98ce25
Kristobal Junta <junta.kristobal@gmail.com>
2019-10-11 09:08:00 +0300

    add solution for year 2015 day 4

---
+++ 2015/d04/p1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+from hashlib import md5
+
+secret = sys.stdin.readline().strip()
+
+i = 0
+
+while True:
+    digest = md5(f"{secret}{i}".encode("utf-8")).hexdigest()
+    if digest.startswith("00000"):
+        print(i)
+        break
+
+    i += 1
